8c190b2a0c59f1dfea8cbda651a90c760ce98524
Andreas E Lundberg <andreas.e.lundberg@gmail.com>
2011-07-30 22:28:04 +0200

    Addition of pre-alpha version NGSbuild script. Filtering of raw reads and creation of binary archives.

---
+++ NGSbuild.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+####################
+# Build binary Mosaik dat files containing filtered raw reads from SOLiD sequencing
+
+# Rutger's perl script for quality filtering, keep =< 10 Phred+33
+# analyze QV distribution, observe the difference between base quality and mapping quality
